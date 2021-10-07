9e4e6457cc385212dc2b11d2a4dd4e9a61a297ad
Mirai Niki <mirainiki@MirainoMac-mini.local>
2021-10-07 12:37:53 +0900

    get cpuinfo from awscli

--- send_cpuinfo/send_cpuinfo.py
+++ send_cpuinfo/send_cpuinfo.py
@@ -6,22 +6,38 @@ import psutil
 import datetime
 from pytz import timezone
 
-TOPIC_ARN = 'arn:aws:sns:ap-northeast-1:xxxxx:xxx-topic'
-
+TOPIC_ARN = 'arn:aws:sns:ap-northeast-1:xxxx:xxx-topic'
+INSTANCE_ID = 'i-xxxx'
+REGION = 'ap-northeast-1'
 now = str(datetime.datetime.now(timezone('Asia/Tokyo')))
 subject = 'CPU利用率_' + now
-client = boto3.client('sns',region_name='ap-northeast-1')
+sns_client = boto3.client('sns',region_name=REGION)
+cloudwatch_client = boto3.client('cloudwatch', region_name=REGION)
 
-# メモリ使用率
-mem = psutil.virtual_memory()
-msg = str(mem.percent)
+# CPU使用率
+cpu_info = cloudwatch_client.get_metric_statistics(
+        Namespace='AWS/EC2',
+        MetricName='CPUUtilization',
+        Dimensions=[
+          {
+            'Name': 'InstanceId',
+            'Value': INSTANCE_ID
+          },
+        ],
+        StartTime=datetime.datetime.utcnow() - datetime.timedelta(seconds=600),
+        EndTime=datetime.datetime.utcnow(),
+        Period=60,
+        Statistics=['Maximum','Average']
+)
 
-response = client.publish(
+msg = str(cpu_info['Datapoints'][0]['Average'])
+response = sns_client.publish(
         TopicArn = TOPIC_ARN,
         Message = msg,
         Subject = subject
         )
 if response['ResponseMetadata']['HTTPStatusCode'] != 200:
-    print("error http status code", response['ResponseMetadata']['HTTPStatusCode'])
+    print(error)
 else:
     print(now,msg,"%")
+
